d2d9106ff720eb5460ecd2d906800ce51554f080
Mattis6666 <sgtmattis@gmail.com>
2020-04-11 18:29:31 +0200

    owo

--- src/utils/checks.ts
+++ src/utils/checks.ts
@@ -4,7 +4,7 @@ export const isMemberHigher = (member1: GuildMember, member2: GuildMember) => {
     return member1.roles.highest.comparePositionTo(member2.roles.highest) > 0;
 };
 
-export const isImageUrl = (url: string) => {
+export const isImageUrl = (url: unknown) => {
     return (
         typeof url === 'string' &&
         ['.png', '.gif', '.webp', '.jpg', '.jpeg'].map(ext => url.includes(ext)).includes(true) &&
