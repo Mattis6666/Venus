dda886b7c36ec5698400a2bba2b0a030a971afd5
Mattis6666 <sgtmattis@gmail.com>
2020-04-29 21:06:39 +0200

    New Commit

--- src/commands/ReactionRoles/reactionrole.ts
+++ src/commands/ReactionRoles/reactionrole.ts
@@ -10,26 +10,28 @@ const callback = async (message: VenusMessage, args: string[], strings: VenusCom
     const [msgID, emojiRaw, ...roleRaw] = args;
 
     const msg = await channel.messages.fetch(msgID).catch(() => null);
-    if (!msg) return wrongSyntax(message, strings.NO_MESSAGE, false);
+    if (!msg) return wrongSyntax(message, strings.NO_MESSAGE);
 
     if (!emoteRegex.test(emojiRaw)) return wrongSyntax(message, strings.NO_EMOJI);
     const emojiID = emojiRaw.slice(emojiRaw.lastIndexOf(':') + 1, emojiRaw.lastIndexOf('>'));
     const emoji = message.client.emojis.cache.get(emojiID);
-    if (!emoji && !msg.reactions.cache.has(emojiID)) return wrongSyntax(message, strings.EMOJI_INACCESSIBLE, false);
+    if (!emoji && !msg.reactions.cache.has(emojiID)) return wrongSyntax(message, strings.EMOJI_INACCESSIBLE);
 
     const role = await getRole(
         message,
         roleRaw.filter(val => !channelRegex.test(val))
     );
-    if (!role) return wrongSyntax(message, strings.NO_ROLE, false);
+    if (!role) return wrongSyntax(message, strings.NO_ROLE);
+    if (message.guild.me && role.position > message.guild.me?.roles.highest.position) return wrongSyntax(message, strings.ROLE_TOO_HIGH);
+    if (role.managed) return wrongSyntax(message, strings.ROLE_MANAGED);
 
     const reacted = emoji ? await msg.react(emoji).catch(() => 'fail') : null;
-    if (reacted === 'fail') return wrongSyntax(message, strings.REACT_FAILED, false);
+    if (reacted === 'fail') return wrongSyntax(message, strings.REACT_FAILED);
 
     const reactionRole =
         (await message.client.database.reactionRoles.findOne({ guild: message.guild.id, channel: channel.id, message: msg.id })) ||
         (await message.client.database.reactionRoles.create({ guild: message.guild.id, channel: channel.id, message: msg.id }));
-    if (reactionRole.emojis.some(reaction => reaction.emoji === emojiID)) return wrongSyntax(message, strings.EMOJI_TAKEN, false);
+    if (reactionRole.emojis.some(reaction => reaction.emoji === emojiID)) return wrongSyntax(message, strings.EMOJI_TAKEN);
     reactionRole.emojis.push({ emoji: emojiID, role: role.id });
     reactionRole.save();
     return message.channel.send(strings.SUCCESS);
